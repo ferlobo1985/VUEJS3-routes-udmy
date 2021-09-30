6232eaad0610ff7296984a2a71b60cd2af78fb73
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-09-30 19:39:30 -0300

    01-Creating routes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
   <app-header></app-header>
         <div class="container">
-          <!-- <home-component></home-component> -->
-          <!-- <articles-main></articles-main> -->
-          <!-- <article-comp></article-comp> -->
-          <contact-component></contact-component>
+          <router-view></router-view>
         </div>
     <app-footer></app-footer>
 </template>
 
 <script>
-  // import HomeComponent from './components/index';
-  // import ArticlesMain from './components/Articles'
-  // import ArticleComp from './components/Articles/article'
-  import ContactComponent from './components/Contact'
-
   export default{
-    components:{
-      // HomeComponent
-      // ArticlesMain
-      // ArticleComp
-      ContactComponent
-    }
+  
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
 
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
+import Articles from './components/Articles';
+import Contact from './components/Contact';
+import Home from './components';
 
 const app =  createApp(App);
 
+const routes = createRouter({
+    history: createWebHistory(),
+    routes:[
+        { path:'/', component: Home },
+        { path:'/articles', component: Articles },
+        { path:'/contact', component: Contact }
+    ]
+});
+
 app.component('app-header',Header);
 app.component('app-footer',Footer);
+app.use(routes)
 app.mount('#app')
