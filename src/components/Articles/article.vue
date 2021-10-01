5feb62ef0dba501d4be029bb90f26e0df7664bdf
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-09-30 22:32:01 -0300

    05-Routes and networking

--- src/components/Articles/article.vue
+++ src/components/Articles/article.vue
@@ -1,53 +1,66 @@
 <template>
 
-<div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-    <div class="col-md-6 px-0">
-      <h1 class="display-4 fst-italic">Incididunt ut labore et dolore magna aliqua</h1>
-      <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </p>
-    </div>
-  </div>
 
-
-  <div class="row g-5">
-    <div class="col-md-8">
-      <h3 class="pb-4 mb-4 fst-italic border-bottom">
-        Dolore magnam aliquam
-      </h3>
-
-
-      <article class="blog-post">
-        <p class="blog-post-meta">December 14, 2020 by <strong>Chris</strong></p>
-
-        <p>Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-        <ul>
-          <li>Nam libero tempore, cum soluta nobis</li>
-          <li>Sed ut perspiciatis unde omnis iste natus</li>
-          <li>Nores eos qui ratione voluptatem sequi ne </li>
-        </ul>
-        <p>I et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quid.</p>
-      </article>
-
-    </div>
-
-    <div class="col-md-4">
-      <div class="position-sticky" style="top: 2rem;">
-        <div class="p-4 mb-3 bg-light rounded">
-          <h4 class="fst-italic">About</h4>
-          <p class="mb-0">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et. </p>
+<router-link to="/articles/3">Go to a different article</router-link>
+<div v-if="Object.keys(article).length !== 0">
+    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+        <div class="col-md-6 px-0">
+            <h1 class="display-4 fst-italic">Incididunt ut labore et dolore magna aliqua</h1>
+            <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </p>
         </div>
-      </div>
     </div>
-  </div>
 
+    <div class="row g-5">
+        <div class="col-md-8">
+            <h3 class="pb-4 mb-4 fst-italic border-bottom">
+               {{ article.title }}
+            </h3>
+
+            <article class="blog-post">
+                <p class="blog-post-meta">{{ article.date}} by <strong>{{ article.author }}</strong></p>
+
+                <div v-html="article.content"></div>
+            </article>
+
+        </div>
+
+        <div class="col-md-4">
+            <div class="position-sticky" style="top: 2rem;">
+                <div class="p-4 mb-3 bg-light rounded">
+                    <h4 class="fst-italic">About</h4>
+                    <p class="mb-0">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 </template>
 
-
 <script>
-  export default {
-    created(){
-     // console.log(this.$router);
-      console.log(this.$route);
-      console.log(this.$route.params.articleId)
-    }
-  }
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            article: {}
+        }
+    },
+    mounted() {
+       this.loadArticleData(this.$route.params.articleId)
+    },
+    methods:{
+      loadArticleData(articleId){
+          axios.get(`http://localhost:3004/articles/${articleId}`)
+            .then(response => {
+                this.article = response.data
+            });
+      }
+    },
+    watch:{
+      $route(to){
+         this.loadArticleData(to.params.articleId)
+      }
+    },
+}
 </script>
--- src/components/Articles/index.vue
+++ src/components/Articles/index.vue
@@ -2,14 +2,15 @@
 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
     <!--  ITEM -->
-    <div class="col">
+    <div class="col" v-for="article in articles" :key="article.id">
         <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
+            <img class="bd-placeholder-img card-img-top" :src="article.img + '?' + Math.random() "/>
             <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <p>{{ article.excerpt }}</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                        <router-link to="/articles/1">
+
+                        <router-link :to="'/articles/' + article.id">
                             <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                         </router-link>
 
@@ -19,126 +20,25 @@
         </div>
     </div>
 
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--  ITEM -->
-    <div class="col">
-        <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" :src="'http://placebeard.it/640/480?' + Math.random() "/>
-            <div class="card-body">
-                <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
 </div>
 </template>
+
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data(){
+            return {
+                articles:[]
+            }
+        },
+        mounted(){
+            axios.get('http://localhost:3004/articles')
+            .then( response => {
+                this.articles = response.data;
+            })
+        }
+    }
+
+</script>
